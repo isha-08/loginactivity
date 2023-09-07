da73589be0521e784eaf5bb68cbc157e2e9534ec
isha-08 <ishaph2017@gmail.com>
2023-09-07 09:43:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/loginactivity/SigninLogin.java
@@ -0,0 +1,14 @@
+package com.example.loginactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SigninLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin_login);
+    }
+}
